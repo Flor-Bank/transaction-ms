862b02235e8efc28f3197eb48ae2fe5bc5b8b8dd
florMignini <flormignini29@gmail.com>
2024-04-16 07:36:49 -0300

    transaction res creation

---
+++ src/transaction/transaction.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, NotImplementedException } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { TransactionService } from './transaction.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+
+@Controller()
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @MessagePattern('createTransaction')
+  create(@Payload() createTransactionDto: CreateTransactionDto) {
+    return this.transactionService.create(createTransactionDto);
+  }
+
+  @MessagePattern('findAllTransaction')
+  findAll() {
+    return this.transactionService.findAll();
+  }
+
+  @MessagePattern('findOneTransaction')
+  findOne(@Payload() id: number) {
+    return this.transactionService.findOne(id);
+  }
+
+  @MessagePattern('changeTransactionStatus')
+  changeTransactionStatus() {
+    /* return this.transactionService.changeTransactionStatus(); */
+    throw new NotImplementedException();
+  }
+}
